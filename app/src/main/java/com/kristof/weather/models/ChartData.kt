6df298657478ff8fb0315f0cd0b24033a047fde7
Kristóf <kristof@Kristof-iMac.local>
2021-03-31 11:45:09 +0200

    Show chart

--- app/src/main/java/com/kristof/weather/models/ChartData.kt
+++ app/src/main/java/com/kristof/weather/models/ChartData.kt
@@ -2,4 +2,4 @@ package com.kristof.weather.models
 
 import com.anychart.chart.common.dataentry.DataEntry
 
-class ChartData(val date: String, val high: Double, val low: Double) : DataEntry()
+class ChartData(val date: String, val high: Number, val low: Number) : DataEntry()
--- app/src/main/java/com/kristof/weather/views/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/com/kristof/weather/views/weather/current/CurrentWeatherFragment.kt
@@ -8,8 +8,14 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import com.anychart.AnyChart
+import com.anychart.AnyChartView
+import com.anychart.chart.common.dataentry.DataEntry
+import com.anychart.charts.Cartesian
+import com.anychart.data.Set
 import com.kristof.weather.MainApplication
 import com.kristof.weather.R
+import com.kristof.weather.models.ChartData
 import com.kristof.weather.models.City
 import com.kristof.weather.models.DailyWeather
 import com.kristof.weather.presenters.WeatherPresenter
@@ -21,7 +27,9 @@ class CurrentWeatherFragment : Fragment(), ICurrentWeatherScreen {
     @Inject
     lateinit var weatherPresenter: WeatherPresenter
 
-    var textView: TextView? = null
+    private lateinit var textView: TextView
+    private lateinit var chartView: AnyChartView
+    private lateinit var cartesian: Cartesian
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,8 +40,36 @@ class CurrentWeatherFragment : Fragment(), ICurrentWeatherScreen {
         val root = inflater.inflate(R.layout.fragment_current, container, false)
         textView = root.findViewById(R.id.text_home)
         val city = arguments?.getString("city")
-        textView?.text = "$city current weather"
+        textView.text = "$city current weather"
+
+        chartView = root.findViewById(R.id.any_chart_view)
+        chartView.setProgressBar(root.findViewById(R.id.progress_bar))
+        initChart()
+        showChart()
         return root
+    }
+
+    private fun initChart() {
+        cartesian = AnyChart.cartesian()
+        cartesian.title("Temperature")
+        cartesian.xAxis(true)
+        cartesian.yAxis(true)
+        cartesian.legend(true)
+        cartesian.yGrid(true).yMinorGrid(true)
+        cartesian.tooltip().titleFormat("{%SeriesName} ({%x})")
+    }
+
+    private fun showChart() {
+        var data = mutableListOf<ChartData>()
+        data.add(ChartData("Jan", 5.8, 4.9))
+
+        val set = Set.instantiate()
+        set.data(data as List<DataEntry>?)
+        val londonData = set.mapAs("{ x: 'date', high: 'high', low: 'low' }")
+
+        var columnLondon = cartesian.rangeColumn(londonData)
+        columnLondon.name("London")
+        chartView.setChart(cartesian)
     }
 
     override fun onAttach(context: Context) {
@@ -43,7 +79,7 @@ class CurrentWeatherFragment : Fragment(), ICurrentWeatherScreen {
 
     override fun onStart() {
         super.onStart()
-        weatherPresenter.attachScreen(this);
+        weatherPresenter.attachScreen(this)
         lifecycleScope.launch(Dispatchers.IO) {
             weatherPresenter.getWeather(City("Budapest"), requireContext())
         }
@@ -51,13 +87,14 @@ class CurrentWeatherFragment : Fragment(), ICurrentWeatherScreen {
 
     override fun onStop() {
         super.onStop()
-        weatherPresenter.detachScreen();
+        weatherPresenter.detachScreen()
     }
 
     override fun showWeather(weather: DailyWeather) {
         lifecycleScope.launch(Dispatchers.Main) {
             //TODO: Display data
-            textView?.text = weather.main.humidity.toString()
+            textView.text = weather.main.humidity.toString()
+            showChart()
         }
     }
 
